a99346ef58db17d5405c1844a4853950609d4651
eddie ruxton <ae.ruxton@gmail.com>
2022-04-18 15:12:55 +0100

    tree form only shows when button clicked

--- components/TreeForm.js
+++ components/TreeForm.js
@@ -1,13 +1,16 @@
-import React, { useState, useContext } from 'react'
+import React, { useRef, useState, useContext, useEffect } from 'react'
 import { TreeContext } from '../pages/TreeContext' //context used to create alert messages
 
 import { TextField, Button } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add';
 
 import { collection, addDoc, serverTimestamp} from '@firebase/firestore' // firebase methods 
 
 import { db } from '../firebase/firebase' // getting the database
 
-const TreeForm = () => {
+const TreeForm = ({ setShowTreeForm }) => {
+  const inputAreaRef = useRef()
+
   const [tree, setTree] = useState({title: '', favourite: false})
 
   const { showAlert } = useContext(TreeContext) // getting the context
@@ -19,14 +22,30 @@ const TreeForm = () => {
 
   // using firebase methods we add a document to the database (the object we built with the form)
   const onSubmit = async () => {
+    setShowTreeForm(false)
     const collectionRef = collection(db, 'Trees')
     const docRef = await addDoc(collectionRef, {...tree, timestamp: serverTimestamp()})
     setTree({title: '', favourite: false})
     showAlert('success', `Tree with id ${docRef.id} added successfully`)
   }
 
+  useEffect(() => {
+    const checkClickArea = (e) => {
+      if(!inputAreaRef.current.contains(e.target)){
+        console.log('clicked outside input');
+        setTree({title: '', favourite: false})
+      } else {
+        console.log('clicked inside input')
+      }
+    }
+    document.addEventListener("mousedown", checkClickArea)
+    return () => {
+      document.removeEventListener("mousedown", checkClickArea)
+    }
+  }, [])
+
   return (
-    <div>
+    <div ref={inputAreaRef}>
       {/* <pre>{JSON.stringify(tree)}</pre> this saves us from using console.log super cool*/}
       <TextField 
       fullWidth
@@ -40,7 +59,7 @@ const TreeForm = () => {
         sx={{ mt: 3 }}
         onClick={onSubmit}
       >
-        New Tree
+      <AddIcon />
       </Button>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 //import styles from '../styles/Home.module.css'
 import { useState } from 'react'
-import { Container, Snackbar, Alert } from '@mui/material'
+import { Container, Snackbar, Alert, Button } from '@mui/material'
 
 import { TreeContext } from './TreeContext'
 import Trees from '../components/Trees'
 import TreeForm from '../components/TreeForm'
 
 export default function Home() {
+  const [showTreeForm, setShowTreeForm] = useState(false)
   const [open, setOpen] = useState(false)
   const [alertType, setAlertType] = useState('succsess')
   const [alertMessage, setAlertMessasge] = useState('')
+
+  const showNewTreeForm = () => {
+    setShowTreeForm(true)
+  }
 
   const showAlert = (type, message) => {
     setAlertType(type)
@@ -28,7 +33,15 @@ export default function Home() {
   return (
     <TreeContext.Provider value={{ showAlert }}>
       <Container>
-        <TreeForm />
+        {showTreeForm ? <TreeForm setShowTreeForm={setShowTreeForm}/> 
+        :  
+        <Button 
+          variant="contained" 
+          sx={{ mt: 3 }}
+          onClick={showNewTreeForm}
+        >
+          New Tree
+        </Button>}
         <Snackbar 
           anchorOrigin={{vertical: 'bottom', horizontal: 'centre'}} 
           open={open} 
