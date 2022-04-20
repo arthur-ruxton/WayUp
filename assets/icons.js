c9adaf744beb82ab30f2af6436069a7d73163c00
eddie ruxton <ae.ruxton@gmail.com>
2022-04-20 19:19:12 +0100

    basic style changes to tree page

--- assets/icons.js
+++ assets/icons.js
@@ -3,9 +3,15 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
+import HomeIcon from '@mui/icons-material/Home';
 
 export { AddIcon }
 export { DeleteIcon }
 export { MoreVertIcon }
 export { CheckIcon }
 export { CloseIcon }
+export { StarIcon }
+export { StarOutlineIcon }
+export { HomeIcon }
