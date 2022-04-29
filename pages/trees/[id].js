4988e8f7caeca5bb7fdc67e521f1e3be14364245
eddie ruxton <ae.ruxton@gmail.com>
2022-04-29 17:40:35 +0100

    show new branch form at the right time

--- pages/trees/[id].js
+++ pages/trees/[id].js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { getDocs, getDoc, doc, collection, query, where } from 'firebase/firestore'
+import { Container, Button } from '@mui/material'
 
 import { getSession } from 'next-auth/client'
 
@@ -7,19 +8,35 @@ import { getSession } from 'next-auth/client'
 import { db } from '../../firebase/firebase'
 import TreeHeader from '../../components/Trees/TreeHeader'
 import BranchList from '../../components/Branches/BranchList'
+import BranchForm from '../../components/Branches/BranchForm'
 
 const Contents = ({ treeProps, branchListProps }) => {
   const [currentTree, setCurrentTree] = useState({})
+  const [showBranchForm, setShowBranchForm] = useState(false)
+
+  const showNewBranchForm = () => {
+    setShowBranchForm(true)
+  }
 
   useEffect(() => {
     setCurrentTree(JSON.parse(treeProps))
   }, [])
 
   return (
-  <div>
+  <Container>
     <TreeHeader currentTree={currentTree} setCurrentTree={setCurrentTree} />
-    <BranchList branchListProps={branchListProps} />
-  </div>
+    <BranchList branchListProps={branchListProps} treeId={currentTree.id}/>
+    {
+      showBranchForm ? <BranchForm setShowBranchForm={setShowBranchForm} treeId={currentTree.id}/> :
+      <Button 
+        variant="contained" 
+        sx={{ mt: 3 }}
+        onClick={showNewBranchForm}
+      >
+        New Branch
+      </Button>
+    }
+  </Container>
   )
 }
 
