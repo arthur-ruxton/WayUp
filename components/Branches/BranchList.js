037be2612d51e2b51dbe85c2d8dfffa8bd975cfa
eddie ruxton <ae.ruxton@gmail.com>
2022-04-29 17:39:57 +0100

    set branch list again when posting and deleting branches

--- components/Branches/BranchList.js
+++ components/Branches/BranchList.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Grid, Item } from '@mui/material';
 
-// import { collection, where, query, onSnapshot } from '@firebase/firestore';
-// import { db } from '../../firebase/firebase';
+import { collection, where, query, onSnapshot } from '@firebase/firestore';
+import { db } from '../../firebase/firebase';
 
 import BranchCard from './BranchCard'
 
-const BranchList = ({branchListProps}) => {
+const BranchList = ({branchListProps, treeId}) => {
 
   // get and set list of trees
   const [branchList, setBranchList] = useState([])
@@ -14,20 +14,26 @@ const BranchList = ({branchListProps}) => {
   useEffect(() => {
     setBranchList(JSON.parse(branchListProps))
   }, [])
- 
+
+  useEffect(() => {
+    if(treeId){
+      const collectionRef = collection(db, "Branches")
+
+      const q = query(collectionRef, where("treeId", "==", treeId))
+
+      const snap = onSnapshot(q, (querySnapshot) => {
+        setBranchList(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+      });
+      return snap
+    }
+  }, [treeId])
 
   return (
     <>
     <Grid sx={{ flexGrow: 1 }} container spacing={2}>
       {branchList.map(branch => (
        <Grid item key={branch.id}>
-          <BranchCard
-          // id={branch.id} 
-          // favourite={branch.favourite} 
-          // timestamp={branch.timestamp} 
-          // title={branch.title}
-          thisBranch={branch}
-          />
+          <BranchCard thisBranch={branch}/>
        </Grid>
       ))}
     </Grid>
