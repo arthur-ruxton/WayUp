c173d24d907e6bafb4cfa13b3ca4ae40a77c5ed6
eddie ruxton <ae.ruxton@gmail.com>
2022-05-01 14:19:31 +0100

    update leaves now working

--- components/Leaves/Leaf.js
+++ components/Leaves/Leaf.js
@@ -1,10 +1,35 @@
-import { doc, deleteDoc } from '@firebase/firestore'
-import { ListItem, IconButton, ListItemText } from '@mui/material'
+import { useState } from 'react'
 
-import { DeleteIcon } from '../../assets/icons'
+import { doc, deleteDoc, updateDoc } from '@firebase/firestore'
+import { ListItem, IconButton, ListItemText, TextField } from '@mui/material'
+
+import { CheckIcon, CloseIcon, DeleteIcon } from '../../assets/icons'
 import { db } from '../../firebase/firebase'
 
 const Leaf = ({thisLeaf, branchId, treeId}) => {
+  const [editing, setEditing] = useState()
+  const [newLeafData, setNewLeafData] = useState({})
+  const [currentLeaf, setCurrentLeaf] = useState(thisLeaf)
+
+   // functionality for editing the leaf conent.
+   const onEditButtonClick = () => {
+    setEditing(true)
+  }
+  const onConentChange = (e) => {
+    setNewLeafData({...currentLeaf, content: e.target.value})
+  }
+  const onSaveButtonClick = async() => {
+    setEditing(false)
+    const docRef = doc(db, "Leaves", currentLeaf.id)
+    const updatedLeaf = {...newLeafData}
+    setCurrentLeaf(updatedLeaf)
+    await updateDoc(docRef, updatedLeaf)
+    setNewLeafData({title: '', favourite: false})
+  }
+  const onCancleEdit = () => {
+    setEditing(false)
+    setNewLeafData({title: '', favourite: false})
+  }
 
   // deletes entire Trees (Projects)
   const onDelete = async(e) => {
@@ -14,21 +39,46 @@ const Leaf = ({thisLeaf, branchId, treeId}) => {
   }
 
   return (
-    <ListItem
-      sx={{ mt: 3, boxShadow: 3 }}
-      style={{ backgroundColor: '#FAFAFA' }}
-      secondaryAction={
+    <>
+    {
+      !editing ? 
+      (
         <>
-          <IconButton  onClick={e => onDelete(e)}>
-            <DeleteIcon/>
-          </IconButton>
+          <ListItem
+            sx={{ mt: 3, boxShadow: 3 }}
+            style={{ backgroundColor: '#FAFAFA' }}
+            secondaryAction={
+              <>
+                <IconButton  onClick={e => onDelete(e)}>
+                  <DeleteIcon/>
+                </IconButton>
+              </>
+            }
+          >
+          <ListItemText
+          onClick={onEditButtonClick}
+          primary={thisLeaf.content}
+          /> 
+          </ListItem>
         </>
+      ) :
+      (<>
+        <TextField 
+          id="outlined-basic" 
+          label={currentLeaf.content} 
+          variant="outlined" 
+          type='text' 
+          onChange={onConentChange}
+        />
+        <IconButton onClick={onSaveButtonClick}>
+          <CheckIcon/>
+        </IconButton>
+        <IconButton onClick={onCancleEdit}>
+          <CloseIcon/>
+        </IconButton>
+        </>)
       }
-    >
-    <ListItemText
-    primary={thisLeaf.content}
-    /> 
-    </ListItem>
+    </>
   )
 }
 
