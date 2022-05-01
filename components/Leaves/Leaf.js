fac563a487ae1f5dc3ab1711d49639dcea6b04fe
eddie ruxton <ae.ruxton@gmail.com>
2022-05-01 18:21:32 +0100

    leaf form replaced with reusable form

--- components/Leaves/Leaf.js
+++ components/Leaves/Leaf.js
@@ -1,34 +1,37 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 
 import { doc, deleteDoc, updateDoc } from '@firebase/firestore'
 import { ListItem, IconButton, ListItemText, TextField } from '@mui/material'
 
 import { CheckIcon, CloseIcon, DeleteIcon } from '../../assets/icons'
 import { db } from '../../firebase/firebase'
+import { DataContext } from '../../pages/DataContext'
 
-const Leaf = ({thisLeaf, branchId, treeId}) => {
+const Leaf = ({thisLeaf}) => {
   const [editing, setEditing] = useState()
-  const [newLeafData, setNewLeafData] = useState({})
+  // const [newLeafData, setNewLeafData] = useState({})
   const [currentLeaf, setCurrentLeaf] = useState(thisLeaf)
+
+  const { newData, setNewData } = useContext(DataContext)
 
    // functionality for editing the leaf conent.
    const onEditButtonClick = () => {
     setEditing(true)
   }
   const onConentChange = (e) => {
-    setNewLeafData({...currentLeaf, content: e.target.value})
+    setNewData({...currentLeaf, text: e.target.value})
   }
   const onSaveButtonClick = async() => {
     setEditing(false)
     const docRef = doc(db, "Leaves", currentLeaf.id)
-    const updatedLeaf = {...newLeafData}
+    const updatedLeaf = {...newData}
     setCurrentLeaf(updatedLeaf)
     await updateDoc(docRef, updatedLeaf)
-    setNewLeafData({title: '', favourite: false})
+    setNewData({title: '', highlight: false})
   }
   const onCancleEdit = () => {
     setEditing(false)
-    setNewLeafData({title: '', favourite: false})
+    setNewData({title: '', highlight: false})
   }
 
   // deletes entire Trees (Projects)
@@ -57,7 +60,7 @@ const Leaf = ({thisLeaf, branchId, treeId}) => {
           >
           <ListItemText
           onClick={onEditButtonClick}
-          primary={thisLeaf.content}
+          primary={thisLeaf.text}
           /> 
           </ListItem>
         </>
@@ -65,7 +68,7 @@ const Leaf = ({thisLeaf, branchId, treeId}) => {
       (<>
         <TextField 
           id="outlined-basic" 
-          label={currentLeaf.content} 
+          label={currentLeaf.text} 
           variant="outlined" 
           type='text' 
           onChange={onConentChange}
