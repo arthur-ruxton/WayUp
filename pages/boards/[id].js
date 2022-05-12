d9223573729569baf4d863bc379bf18e7f17fe47
eddie ruxton <ae.ruxton@gmail.com>
2022-05-12 16:46:37 +0100

    adding in my existing dnd ui but splitting the data into three objects to more closely represent my project database

--- pages/boards/[id].js
+++ pages/boards/[id].js
@@ -1,104 +1,153 @@
-import React, { useState, useEffect } from 'react'
-import { getDocs, getDoc, doc, collection, query, where } from 'firebase/firestore'
-import { Container, IconButton } from '@mui/material'
+import { useState } from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd-next'
 
-import { getSession } from 'next-auth/client'
+import Container from '@mui/material/Container';
 
-// file system imports 
-import { AddIcon } from '../../assets/icons'
-import { db } from '../../firebase/firebase'
-import BoardHeader from '../../components/Boards/BoardHeader'
-import CardList from '../../components/Cards/CardList'
-import NewDataForm from '../../components/NewDataForm'
+// import initialData from '../../initial-data'
+import { initialItemData, initialCardData, initialBoardData } from '../../initial-data'
+import ListContainer from '../../components/dnd/ListContainer';
 
-const Contents = ({ boardProps, cardListProps, itemListProps }) => {
-  const [currentBoard, setCurrentBoard] = useState({})
-  const [showForm, setShowForm] = useState(false)
+export default function Home() {
+  // const [data, setData] = useState(initialData)
+  const [itemData, setItemData] = useState(initialItemData)
+  const [cardData, setCardData] = useState(initialCardData)
+  const [boardData, setBoardData] = useState(initialBoardData)
 
-  const showNewForm = () => {
-    setShowForm(true)
-  }
+  // function for persisting data when reordering
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result
 
-  useEffect(() => {
-    setCurrentBoard(JSON.parse(boardProps))
-  }, [])
-
-  return (
-  <Container>
-    <BoardHeader currentBoard={currentBoard} setCurrentBoard={setCurrentBoard} />
-    <CardList 
-    boardProps={boardProps}
-    cardListProps={cardListProps} 
-    itemListProps={itemListProps} 
-    boardId={currentBoard.id}
-    />
-    {
-      showForm ? 
-      <NewDataForm 
-        setShowForm={setShowForm} 
-        boardId={currentBoard.id}
-        dataCollection="Cards" 
-        type="card" 
-        maxLength={26}
-      />
-      :
-      <IconButton 
-        variant="contained" 
-        sx={{ mt: 3 }}
-        onClick={showNewForm}
-      >
-        <AddIcon />
-      </IconButton>
+    if(!destination) {
+      return
     }
-  </Container>
-  )
-}
+    // check if the items location has changed
+    if(destination.droppableId === source.droppableId &&
+      destination.index === source.index){
+        return
+      }
 
-export default Contents
+    if(type === 'card'){
+      // const newCardsOrder = Array.from(data.cardsOrder)
+      const newCardsOrder = Array.from(boardData.cardsOrder)
+      newCardsOrder.splice(source.index, 1)
+      newCardsOrder.splice(destination.index, 0, draggableId)
+      const updatedData = {
+        ...boardData,
+        cardsOrder: newCardsOrder
+      }
+      // setData(updatedData)
+      setBoardData(updatedData)
+      return
+    }
+    
+    // ----------> reorder the itemIds 
 
-// getServerSideProps allows server side rendering, I'm not super clear on the difference this makes.
-  export const getServerSideProps = async (context) => {
+      // 1. retrive start collumn from state 
+      // const startCard = data.cards[source.droppableId]
+      const startCard = cardData.cards[source.droppableId]
+      // 2. retrive destination collumn from state 
+      // const finishCard = data.cards[destination.droppableId]
+      const finishCard = cardData.cards[destination.droppableId]
 
-    const session = await getSession(context)
-
-    if(!session){
-      return {
-        redirect: {
-          destination: '/api/auth/signin',
-          permanent: false,
+    // case 1. reodering items within a cards
+    if(startCard === finishCard){
+      // copy array of start cardss itemIds
+        const newItemIds = Array.from(startCard.itemIds)
+        //remove one item from source index 
+      newItemIds.splice(source.index, 1)
+        // insert itemId at the destination index
+      newItemIds.splice(destination.index, 0, draggableId)
+        // create newCards object with the updated itemIds array
+      const newCard = {
+        ...startCard,
+        itemIds: newItemIds
+      }
+        // replace old cards with new cards 
+      const newData = {
+        // ...data,
+        ...cardData,
+        cards: {
+          // ...data.cards, 
+          ...cardData.cards, 
+          [newCard.id]: newCard
         }
       }
+        // setData(newData)
+        setCardData(newData)
+        return;
     }
 
-    const id = context.params.id
-  
-    const docRef = doc(db, 'Boards', id)
-    const docSnap = await getDoc(docRef)
-
-    const cardsRef = collection(db, "Cards")
-    const cardsQ = query(cardsRef, where("boardId", "==", id))
-    const cardsQuerySnapshot = await getDocs(cardsQ)
-    let cardList = []
-    cardsQuerySnapshot.forEach((doc) => {
-      cardList.push({ ...doc.data(), id: doc.id })
-    })
-    //console.log('card list', cardList)
-
-    const itemsRef = collection(db, "items")
-    //const itemsQ = query(itemsRef, where("treeId", "==", id))
-    const itemsQuerySnapshot = await getDocs(itemsRef)
-    let itemList = []
-    itemsQuerySnapshot.forEach((doc) => {
-      itemList.push({ ...doc.data(), id: doc.id })
-    })
-    // console.log('item list server side', itemList)
-  
-    return {
-      props: { 
-        session,
-        boardProps: JSON.stringify({ ...docSnap.data(), id: docSnap.id, timestamp: docSnap.data().timestamp?.toDate().getTime() }) || null,
-        cardListProps: JSON.stringify(cardList) || [],
-        itemListProps: JSON.stringify(itemList) || [],
-      },
+    // case 2. handle moving items between cards
+    // start by creating copy of start/finish cards itemId's array
+    const newStartCardItemIds = Array.from(startCard.itemIds)
+    const newFinishCardItemIds = Array.from(finishCard.itemIds)
+    //remove one item from source index 
+    newStartCardItemIds.splice(source.index, 1)
+    // insert itemId at the destination index
+    newFinishCardItemIds.splice(destination.index, 0, draggableId)
+    // create newStartCards/newFinishCards object with the updated itemIds array
+    const newStartCard = {
+      ...startCard,
+      itemIds: newStartCardItemIds
     }
+    const newFinishCard = {
+      ...finishCard,
+      itemIds: newFinishCardItemIds
+    }
+      // replace old cardss with new cardss
+      const newData = {
+        // ...data,
+        ...cardData,
+        cards: {
+          // ...data.cards,
+          ...cardData.cards, 
+          [newStartCard.id]: newStartCard,
+          [newFinishCard.id]: newFinishCard
+        }
+      }
+      // setData(newData)
+      setCardData(newData)
+        return;
   }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd} >
+      {/*-- here we wrap everything in a droppable, allowing us to make cardss draggable --*/}
+      <Droppable 
+      droppableId="total-droppable" 
+      direction="horizontal" 
+      // draggables can only be dropped if start/finish droppable have same type! VERY USEFUL
+      type="card"
+      >
+        {(provided) => {
+          return (
+            <Container 
+              ref={provided.innerRef} 
+              {...provided.droppableProps}
+              sx={{display:'flex',  maxWidth:"full"}}
+              >
+              {
+                // data.cardsOrder.map((cardId, index) => {
+                boardData.cardsOrder.map((cardId, index) => {
+                  // const card = data.cards[cardId]
+                  const card = cardData.cards[cardId]
+                  return (
+                    <ListContainer
+                      key={card.id} 
+                      card={card} 
+                      // itemMap={data.items}
+                      itemMap={itemData.items} 
+                      index={index}
+                    />
+                  )
+                })
+              }
+              {provided.placeholder}
+            </Container>
+          )
+        }
+      }
+      </Droppable>
+    </DragDropContext>
+  )
+}
