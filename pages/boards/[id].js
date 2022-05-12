fe4afdc14f586cd9a1e40913d9ed0d5618486c44
eddie ruxton <ae.ruxton@gmail.com>
2022-05-12 18:25:44 +0100

    bringing ssprops back into my board page, using it to render the title

--- pages/boards/[id].js
+++ pages/boards/[id].js
@@ -1,15 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import { getDocs, getDoc, doc, collection, query, where } from 'firebase/firestore'
 import Container from '@mui/material/Container';
 
-import DragDropContainer from '../../components/dnd/DragDropContainer'
+import { getSession } from 'next-auth/client'
 
-import { initialBoardData } from '../../initial-data'
+import { db } from '../../firebase/firebase'
+// import DragDropContainer from '../../components/dnd/DragDropContainer'
+// import { initialBoardData } from '../../initial-data'
+import BoardHeader from '../../components/Boards/BoardHeader'
 
-export default function Home() {
+export default function Home({boardProps, cardListProps, itemListProps}) {
+  const [currentBoard, setCurrentBoard] = useState({})
+
+  useEffect(() => {
+    setCurrentBoard(JSON.parse(boardProps))
+  }, [])
 
   return (
     <Container  sx={{display:'flex', flexDirection:"column",  maxWidth:"full"}}>
-      <h1>{initialBoardData.text}</h1>
-      <DragDropContainer />
+      <BoardHeader currentBoard={currentBoard} setCurrentBoard={setCurrentBoard} />
+      {/* <DragDropContainer /> */}
     </Container>
   )
+}
+
+// getServerSideProps allows server side rendering, I'm not super clear on the difference this makes.
+export const getServerSideProps = async (context) => {
+
+  const session = await getSession(context)
+
+  if(!session){
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      }
+    }
+  }
+
+  const id = context.params.id
+
+  const docRef = doc(db, 'Boards', id)
+  const docSnap = await getDoc(docRef)
+
+  const cardsRef = collection(db, "Cards")
+
+  const cardsQ = query(cardsRef, where("boardId", "==", id))
+  const cardsQuerySnapshot = await getDocs(cardsQ)
+  let cardList = []
+  cardsQuerySnapshot.forEach((doc) => {
+    cardList.push({ ...doc.data(), id: doc.id })
+  })
+  //console.log('card list', cardList)
+
+  const itemsRef = collection(db, "items")
+  const itemsQ = query(itemsRef, where("boardId", "==", id))
+  const itemsQuerySnapshot = await getDocs(itemsQ)
+  let itemList = []
+  itemsQuerySnapshot.forEach((doc) => {
+    itemList.push({ ...doc.data(), id: doc.id })
+  })
+  // console.log('item list server side', itemList)
+
+  return {
+    props: { 
+      session,
+      boardProps: JSON.stringify({ ...docSnap.data(), id: docSnap.id, timestamp: docSnap.data().timestamp?.toDate().getTime() }) || null,
+      cardListProps: JSON.stringify(cardList) || [],
+      itemListProps: JSON.stringify(itemList) || [],
+    },
+  }
 }
